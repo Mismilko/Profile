0e293878f7473d90fda5be359782ef6febe0bb16
M.I.S <irina.morozova.21@inbox.ru>
2023-07-11 16:50:09 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.Passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.Phone = "+7(999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+    }
+
+
+    }
---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+    public String name; // поле для имени
+    public String Passport;
+    public String patronymic;
+    public String Phone;
+    public String surname;
+    public FormDate birthday;
+    public boolean subscription;
+
+
+}
